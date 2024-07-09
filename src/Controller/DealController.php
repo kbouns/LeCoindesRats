ca8bc87cd247c56f51b888cfcdbc1749656781f2
kbouns <bounseng.kevin@gmail.com>
2024-07-09 17:32:09 +0200

    Maj deal

--- src/Controller/DealController.php
+++ src/Controller/DealController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Deal;
 use App\Entity\Comment;
+use App\Repository\DealRepository;
 use App\Form\CommentType;
+use App\Form\DealType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class DealController extends AbstractController
 {
